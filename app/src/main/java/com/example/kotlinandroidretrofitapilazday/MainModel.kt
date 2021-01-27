41283f1d73b1ece590eb549d214ef0f90435235b
Kevin williams <kevinwilliamsubk1@gmail.com>
2021-01-27 18:06:04 +0700

    Make Model/Data Class

---
+++ app/src/main/java/com/example/kotlinandroidretrofitapilazday/MainModel.kt
@@ -0,0 +1,3 @@
+package com.example.kotlinandroidretrofitapilazday
+
+data class MainModel (val id: Int, val title: String, val url: String) // tambahkan data
--- app/src/main/java/com/example/kotlinandroidretrofitapilazday/retrofit/ApiEndPoint.kt
+++ app/src/main/java/com/example/kotlinandroidretrofitapilazday/retrofit/ApiEndPoint.kt
@@ -1,9 +1,11 @@
 package com.example.kotlinandroidretrofitapilazday.retrofit
 
+import com.example.kotlinandroidretrofitapilazday.MainModel
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiEndPoint {
     // ini untukInterface
     @GET("photos")
-    fun getPhotos()
+    fun getPhotos(): Call<List<MainModel>> // mengabil dari data model
 }
