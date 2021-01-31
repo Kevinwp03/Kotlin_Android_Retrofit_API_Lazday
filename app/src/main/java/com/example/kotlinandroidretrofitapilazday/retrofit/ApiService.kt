cd5d0fb4f631859b1ed573aae7f181bbeff353d7
Kevin williams <kevinwilliamsubk1@gmail.com>
2021-01-31 11:51:13 +0700

    Melihat Error dengan OkHttp Interceptor

--- app/src/main/java/com/example/kotlinandroidretrofitapilazday/retrofit/ApiService.kt
+++ app/src/main/java/com/example/kotlinandroidretrofitapilazday/retrofit/ApiService.kt
@@ -1,5 +1,7 @@
 package com.example.kotlinandroidretrofitapilazday.retrofit
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -9,8 +11,19 @@ object ApiService {
     val BASE_URL: String = "https://demo.lazday.com/rest-api-sample/"
     val endPoint: ApiEndPoint //diambil dari class ApiEndPoint
         get() {
+            //
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            //buat clientnya
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build()
+
+            //
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client) // paggil cliennya, setelah BaseUrl
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
